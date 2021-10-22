04fb51dcc90e62dbe05948b8699f256f9709fe6e
Alaa-Dawoud <48125535+Alaa-Dawoud@users.noreply.github.com>
2021-10-22 09:39:44 +0200

    Add files via upload

---
+++ inc/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WeVote</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+	<?php include 'navbar.php';?>
