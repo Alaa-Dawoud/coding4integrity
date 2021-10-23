6a4ccacc8b937159e37fb23f663d9bce1be2efdf
Alaa-Dawoud <48125535+Alaa-Dawoud@users.noreply.github.com>
2021-10-23 15:27:24 +0200

    Add files via upload

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,3 +1,49 @@
+<!--
+this is nav bar for vote and candidate users 
+for guest is below this one
+-->
+<?php include 'config/config.php';?>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="<?php echo '/wevote/candidates.php'; ?>">WeVote</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor02">
+      <ul class="navbar-nav ms-auto">
+        <!--This item for candidate users only-->
+        <li class="nav-item">
+          <a class="nav-link" href="#">View My Profile</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/wevote/<?php echo BASE_URL; ?>">Candidates List</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/wevote/wvow">Leaderboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Program critize</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link btn btn-danger" href="#">Appeals</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+<!--
+This is nav bar for welcoming page
+we will use php to determine which one 
+to use according to user(guest - vote - candidate)
+vote and candidate user will have same navbar
+which is the above one
+-->
+<!--
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">WeVote</a>
@@ -5,7 +51,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <div class="collapse navbar-collapse" id="navbarColor02">
       <ul class="navbar-nav ms-auto">
         <li class="nav-item">
           <a class="nav-link" href="#">Log In</a>
@@ -17,3 +63,5 @@
     </div>
   </div>
 </nav>
+
+-->
