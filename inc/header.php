f2b1827d7a84344efb1378b04979429fd087556b
Alaa-Dawoud <48125535+Alaa-Dawoud@users.noreply.github.com>
2021-10-25 08:13:02 +0200

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>WeVote</title>
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	<link rel="stylesheet" href="https://bootswatch.com/5/cosmo/bootstrap.min.css">
 </head>
 <body>
