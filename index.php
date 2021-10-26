1fe55c31e67980ef12b1b26f78e0547c9172cfc8
Alaa-Dawoud <48125535+Alaa-Dawoud@users.noreply.github.com>
2021-10-26 09:51:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
 <?php include 'inc/header.php';?>
-	<h1>Hello To our Website</h1>
+
+<!-- Introduction -->	
+<section class="colored-section" id="title">
+      <!-- Title -->
+      <div class="row">
+        <div class="col-lg-5">
+              <h3>We Are Voting Team</h1>
+              <h1 class="big-heading">Creative Voting</h2>
+              <h3>We help you find the best candidate to vote for by providing you his electoral program</p>
+              <p>It is prepared for your voting organization whether parliament, club and any voting system</p>
+        </div>
+      </div>
+    </div>
+</section>
+<!-- Features -->
+<section id="features">
+    <div class="container">
+      <div class="row">
+        <div class="features-box col-lg-4">
+          <h3 class="feature-text">Elite Candidates</h3>
+          <p>So easy to use, we let you choose among the best candidates</p>
+        </div>
+        <div class="features-box col-lg-4">
+          <h3 class="feature-text">Smart comparision</h3>
+          <p>We make you a leaderboard to compare between candidates and every candidate electoral program</p>
+        </div>
+        <div class="features-box col-lg-4">
+          <h3 class="feature-text">Put your opinion</h3>
+          <p>We let you critisize every candidate's program and you can make appeals aginst them</p>
+        </div>
+      </div>
+    </div>
+ </section>
+
+
 <?php include 'inc/footer.php';?>
